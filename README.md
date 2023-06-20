3a81c405f9f0baef8ee780d94686edc0985d7170
ryoalstarsoft <felip.augustine.ramirez@gmail.com>
2023-06-19 21:53:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Build a Chat Application using React, Redux, Redux-Saga and Web Sockets
+
+![](./result.gif)
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+License: MIT
---
+++ src/reducers/messages.js
@@ -0,0 +1,18 @@
+const messages = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_MESSAGE':
+    case 'MESSAGE_RECEIVED':
+      debugger;
+      return state.concat([
+        {
+          message: action.message,
+          author: action.author,
+          id: action.id
+        }
+      ])
+    default:
+      return state
+  }
+}
+
+export default messages
